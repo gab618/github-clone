9edf5206476e4a6d878ee3983c0fa62abdf76682
Gabriel <knuckles4x@gmail.com>
2020-08-06 02:57:23 -0300

    Profile Data on left side

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,9 +1,31 @@
 import React from "react";
 
-import { Container } from "./styles";
+import { Container, Main, LeftSide, RightSide } from "./styles";
+import ProfileData from "../../components/ProfileData";
 
 const Profile: React.FC = () => {
-  return <div />;
+  return (
+    <Container>
+      <Main>
+        <LeftSide>
+          <ProfileData
+            username={"gab618"}
+            name={"Gabriel"}
+            avatarUrl={
+              "https://avatars0.githubusercontent.com/u/24815192?s=460&u=48532a8215af22a51335cad56601ffa948b2a89e&v=4"
+            }
+            followers={3333}
+            following={777}
+            company={"Panela Craft Inc."}
+            location={"São Paulo, Brazil"}
+            email={"knuckles4x@gmail.com"}
+            blog={"linkedin.com/in/grocha97"}
+          />
+        </LeftSide>
+        <RightSide></RightSide>
+      </Main>
+    </Container>
+  );
 };
 
 export default Profile;
